4896d9289fc26b813a4362222d1d05171c9307a0
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-06-14 20:22:02 +0530

    modified files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Responsive-Landing-Page",
   plugins: [react()],
   server: {
     watch: {
